8af502da9c3352894387fc5b2a8f9a8c7ab4657a
mihai <saftacatalinmihai@gmail.com>
2016-08-19 18:19:25 +0300

    Euler64 - fixed accumulate -> the sq match in where was the issue

--- src/Euler64.hs
+++ src/Euler64.hs
@@ -19,12 +19,12 @@ next_frac a0 (a, sq, b) =
         na = ((a0 + b) `div` next_a)
         next_b = na * next_a - b
 
-accumulate a0 a sq b acc | trace ("accumulate " ++ show acc ++ " " ++ show a ++ " " ++ show sq ++ " " ++ show b ) False = undefined
+-- accumulate a0 a sq b acc | trace ("accumulate " ++ show acc ++ " " ++ show a ++ " " ++ show sq ++ " " ++ show b ) False = undefined
 accumulate a0 a sq b acc
     | (loop_found acc) = acc
-    | otherwise = accumulate a0 na sq nb (a1:acc)
+    | otherwise = (accumulate a0 na sq nb (a1:acc))
     where
-        (a1, (na, sq, nb)) = next_frac a0 (a, sq, b)
+        (a1, (na, _, nb)) = (next_frac a0 (a, sq, b))
 
 split l = splitAt (((length l) + 1) `div` 2) l
 loop_found l =
